aaa04037b643512d43e7425d3fb81e439500b390
narihy <maheninarandrianarisoa@gmail.com>
2023-06-23 11:21:35 +0200

    ajout

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+
+    <link href="{{ asset('bootstrap/dist/css/bootstrap.min.css')}}" rel="stylesheet">
+
+    
+      <!-- Vendor CSS Files -->
+    <link href="{{ asset('as/vendor/animate.css/animate.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('as/vendor/aos/aos.css')}}" rel="stylesheet">
+    <link href="{{ asset('as/vendor/remixicon/remixicon.css')}}" rel="stylesheet">
+    <link href="{{ asset('as/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('as/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('as/vendor/remixicon/remixicon.css')}}" rel="stylesheet">
+    <link href="{{ asset('as/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link rel="stylesheet" href="{{ asset('as/css/style.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/b/style.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/b/styles.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/head.css')}}">
+    
+    
+
+
+</head>
+<body id="dark">
+
+
+<header class="navbar navbar-dark fixed-top " id="purple">
+        
+          
+    
+        <div class="container-fluid">
+        <img src="{{asset('img/mcar.jpg')}}" alt="logo scout" width="200px" height="57px" >
+      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbarLight" aria-controls="offcanvasNavbarLight" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbarLight" aria-labelledby="offcanvasNavbarLightLabel" style="background-color:  #692d91;">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasNavbarLightLabel">Offcanvas</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+          </ul>
+          <form class="d-flex mt-3" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </div>
+
+</header>
+    
+
+ 
+        @yield('content')
+
+        <div id="preloader"></div>
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<footer class="py-3" id="purple">
+          
+          <p  class="text-center text-muted">&copy; <a id="white">2022, Powered by Sampati X e B</a> </p>
+</footer>
+  <script src="{{ asset('as/vendor/purecounter/purecounter_vanilla.js')}}"></script>
+  <script src="{{ asset('as/vendor/aos/aos.js')}}"></script>
+  <script src="{{ asset('as/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{ asset('as/vendor/swiper/swiper-bundle.min.js')}}"></script>
+  <script src="{{ asset('as/vendor/php-email-form/validate.js')}}"></script>
+
+  <!-- Template Main JS File -->
+
+  <script src="{{ asset('as/js/main.js')}}"></script>
+ 
+</body>
+    
---
+++ resources/views/scout/index.blade.php
@@ -0,0 +1,125 @@
+@extends('layout')
+@section('title', 'Scout')
+
+@section('content')
+
+    <section id="hero" class="d-flex justify-content-center align-items-center">
+    <div class="container position-relative" data-aos="zoom-in" data-aos-delay="100">
+      
+      <h1>Donner aux jeunes  <br> les compétences et <br> les moyens de créer <br>un monde meilleur</h1>
+      <a href="" class="btn-get-started">Commencer à visiter</a>
+    </div>
+  </section>
+  
+  <div class="container">
+  <main id="main">
+
+<section id="about" class="about">
+  <div class="container" data-aos="fade-up">
+
+    <div class="row">
+      <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+        <img src="{{asset('img/news/chef.jpg')}}" class="img-fluid" alt="">
+      </div>
+      <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
+        <h3 class="orange">Rindra andrian</h3>
+        <p class="soratra"> Le scoutisme est l'endroit où j'ai appris à surmonter ma propre force, mon esprit et mes émotions : le scoutisme est une école de la vie. Dans le scoutisme j'ai affronté : la honte, la peur, la fatigue. Quand vous pensez que c'est la limite, c'est là que commence la chose la plus délicieuse. Le Scoutisme m'a appris à servir Dieu et à aimer mon pays : c'est la base que M. Baden Powell a voulu transmettre. Il n'y a pas de meilleure vie que de servir Dieu, d'aider ses amis et d'aimer son pays. Mais est-ce que ça veut dire que je vais me mépriser ? Non, vous ne pouvez pas vraiment aimer les autres tant que vous n'avez pas appris à "vous aimer".</p>
+        
+        
+
+      </div>
+    </div>
+
+  </div>
+</section>
+<section id="counts" class="counts section-bg">
+  <div class="container">
+
+    <div class="row counters">
+
+      <div class="col-lg-3 col-6 text-center">
+        <span data-purecounter-start="0" data-purecounter-end="10000000" data-purecounter-duration="1" class="purecounter"></span>
+        <p>MONDIALE</p>
+      </div>
+
+      <div class="col-lg-3 col-6 text-center">
+        <span data-purecounter-start="0" data-purecounter-end="200000" data-purecounter-duration="1" class="purecounter"></span>
+        <p>AFRICAINE</p>
+      </div>
+
+      <div class="col-lg-3 col-6 text-center">
+        <span data-purecounter-start="0" data-purecounter-end="100000" data-purecounter-duration="1" class="purecounter"></span>
+        <p>MADAGASCAR</p>
+      </div>
+
+      <div class="col-lg-3 col-6 text-center">
+        <span data-purecounter-start="0" data-purecounter-end="101" data-purecounter-duration="1" class="purecounter"></span>
+        <p>X e B</p>
+      </div>
+
+    </div>
+
+  </div>
+</section>
+
+<div class="container">
+<section id="trainers" class="trainers">
+  <div class="container" data-aos="fade-up">
+
+    <div class="row" data-aos="zoom-in" data-aos-delay="100">
+      <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+        <div class="member">
+          <img src="{{asset('img/acc/Tsiparampitariha.jpg')}}" class="img-fluid" alt="">
+          <div class="member-content">
+            <h4>Tsipara mpitariha</h4>
+            <span>Mpiandraikitra</span>
+            <p>
+            "Etre membre scout nous donne un bon privilège, vous apprènne à mieux connaitre le monde y compris vous même."
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+        <div class="member">
+          <img src="{{asset('img/acc/mianja.jpg')}}" class="img-fluid" alt="">
+          <div class="member-content">
+            <h4>Mianja RAZAFIMANDIMBY</h4>
+            <span>Mpiandraikitra</span>
+            <p>
+            "Venez nous rejoindre, le scoutisme nous apprend beaucoup de chose"
+            </p>
+           
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+        <div class="member">
+          <img src="{{asset('img/acc/ito.jpg')}}" class="img-fluid" alt="">
+          <div class="member-content">
+            <h4>Zabady mazoto</h4>
+            <span>Beazina</span>
+            <p>
+            "Le scoutisme nous rend heureux, qu'est-ce-que vous attendez pour nous rejoindre"
+            </p>
+            
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    
+
+  
+        <div class="cent">
+        <a href="" class="btn btn-outline-success">Cliquer ici pour voir plus d'information</a>
+        </div>
+          
+      </div>
+
+</section>
+    </div>
+    </div>
+@endsection

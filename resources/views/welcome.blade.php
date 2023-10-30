032e2f5435d306e1149135bc6acc713a86bb9191
Antsa <115356983+Antsa-Fi30@users.noreply.github.com>
2023-10-30 10:54:35 +0300

    Update welcome.blade.php
    Delete Hello World from welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -128,9 +128,7 @@ var(--tw-ring-color);box-shadow:var(--tw-ring-offset-shadow), var(--tw-ring-shad
                     fill="#FF2D20" />
             </svg>
         </div>
-        <div>
-            <h1>HELLO WORLD</h1>
-        </div>
+      
 
         <div class="mt-16">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
